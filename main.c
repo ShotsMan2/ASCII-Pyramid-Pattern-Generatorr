4a62659c6684baa6b0db22f17d8f4758f294592a
Selim Efe <uysalselimefe@gmail.com>
2026-01-19 15:38:52 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 
 int main()
 {
-    for(i=1;i<=5;i++){
-        for(j=1;j<2*i;j++){
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j < 2 * i; j++)
+        {
             printf("*");
         }
         printf("\n");
